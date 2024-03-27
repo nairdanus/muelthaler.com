1c9b5518415c7423dabf9c81072cd3aa615a6cb8
Adrian Mülthaler <adrian.muelthaler@campus.lmu.de>
2024-03-27 16:14:24 +0100

    renamed cv url and added link to contact form

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,5 @@ from . views import home, contact, open_pdf
 urlpatterns = [
     path('', home, name='home'),
     path('contact/', contact, name='contact'),
-    path('open_pdf/', open_pdf, name='open_pdf'),
+    path('cv/', open_pdf, name='open_pdf'),
 ]
