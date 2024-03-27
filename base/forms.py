5b187b6fc24ce4096edf98a0b0f4f6bc2e438ff7
Adrian Mülthaler <adrian.muelthaler@campus.lmu.de>
2024-03-27 13:47:36 +0100

    minor style update

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 from captcha.fields import CaptchaField
+from django.utils.translation import gettext_lazy
+
 
 class PdfAccessForm(forms.Form):
-    captcha = CaptchaField()
+    captcha = CaptchaField(
+        error_messages={"invalid": gettext_lazy("Invalid CAPTCHA")},
+
+    )
