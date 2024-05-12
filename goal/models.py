7bbc60ea69df362d6851f9e8de93c98bb15ba295
adrian <nairdanus@gmail.com>
2024-05-12 18:40:06 +0200

    added prereport

--- goal/models.py
+++ goal/models.py
@@ -12,6 +12,7 @@ class Player(models.Model):
 class Match(models.Model):
     opponent = models.CharField(max_length=100, default="Unbekannter Gegner")
     report = models.TextField(default="", blank=True)
+    pre_report = models.TextField(default="", blank=True)
     date = models.DateField(default=timezone.now)
     players = models.ManyToManyField(Player, through='Goal', related_name='matches')
     counter_goals = models.PositiveIntegerField(default=0)
