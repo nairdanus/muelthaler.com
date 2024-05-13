08d02c15bc421eda6e745cfe683b66685fdeeda1
adrian <nairdanus@gmail.com>
2024-05-13 15:56:49 +0200

    Another try

--- goal/admin.py
+++ goal/admin.py
@@ -6,20 +6,3 @@ from django import forms
 admin.site.register(Player)
 admin.site.register(Goal)
 admin.site.register(Match)
-
-
-# class MatchAdminForm(forms.ModelForm):
-#     class Meta:
-#         model = Match
-#         fields = '__all__'  # Or specify the fields you want to include
-
-#     def clean_pre_report(self):
-#         print(self.cleaned_data)
-#         return self.cleaned_data["pre_report"]
-
-
-# class MatchAdmin(admin.ModelAdmin):
-#     form = MatchAdminForm
-
-
-# admin.site.register(Match, MatchAdmin)
--- goal/models.py
+++ goal/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.utils import timezone
 
+from django.utils.html import mark_safe
+
+
+class MarkdownHTMLField(models.TextField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def clean(self, value, model_instance):
+        value = super().clean(value, model_instance)
+        return mark_safe(html_content)
+
 
 class Player(models.Model):
     name = models.CharField(max_length=100)
@@ -11,8 +22,8 @@ class Player(models.Model):
 
 class Match(models.Model):
     opponent = models.CharField(max_length=100, default="Unbekannter Gegner")
-    pre_report = models.TextField(default="", blank=True, validators=[])
-    report = models.TextField(default="", blank=True, validators=[])
+    pre_report = MarkdownHTMLField(default="", blank=True)
+    report = MarkdownHTMLField(default="", blank=True)
     date = models.DateField(default=timezone.now)
     players = models.ManyToManyField(Player, through='Goal', related_name='matches')
     counter_goals = models.PositiveIntegerField(default=0)
