f498bdee149a9c1a086a16e832337093a450394e
adrian <nairdanus@gmail.com>
2024-05-13 14:50:14 +0200

    Small model changes

--- goal/admin.py
+++ goal/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Player, Goal, Match
+from django import forms
+
 
 admin.site.register(Player)
 admin.site.register(Goal)
-admin.site.register(Match)
+# admin.site.register(Match)
+
+
+class YourModelAdminForm(forms.ModelForm):
+    class Meta:
+        model = Match
+        fields = '__all__'  # Or specify the fields you want to include
+
+    def clean_pre_report(self):
+        return self.cleaned_data.get('pre_report', '')
+
+
+class YourModelAdmin(admin.ModelAdmin):
+    form = YourModelAdminForm
+
+
+admin.site.register(Match, YourModelAdmin)
--- goal/models.py
+++ goal/models.py
@@ -11,12 +11,16 @@ class Player(models.Model):
 
 class Match(models.Model):
     opponent = models.CharField(max_length=100, default="Unbekannter Gegner")
-    report = models.TextField(default="", blank=True)
     pre_report = models.TextField(default="", blank=True)
+    report = models.TextField(default="", blank=True)
     date = models.DateField(default=timezone.now)
     players = models.ManyToManyField(Player, through='Goal', related_name='matches')
     counter_goals = models.PositiveIntegerField(default=0)
     group_picture = models.ImageField(upload_to='group_pictures/', blank=True, null=True)
+
+    def __str__(self):
+        return self.opponent
+    
 
 class Goal(models.Model):
     match = models.ForeignKey(Match, on_delete=models.CASCADE, default=None)
